f5ed4c5a49b34e6b2f5536ed964c9483b9ca7082
ananta0705 <ananta07fadhila@gmail.com>
2023-11-07 01:56:02 +0700

    footer

--- resources/views/admin/template/footer.blade.php
+++ resources/views/admin/template/footer.blade.php
@@ -2,7 +2,7 @@
                 <div class="container">
                     <div class="row align-items-center flex-row-reverse">
                         <div class="col-md-12 col-sm-12 text-center">
-                            Copyright © <span id="year"></span> <a href="javascript:void(0);">Zanex</a>. Designed with <span class="fa fa-heart text-danger"></span> by <a href="javascript:void(0);"> Spruko </a> All rights reserved
+                            Proyek 2 © <span id="year"></span> <a href="javascript:void(0);">Kelompok 1</a>. Designed with <span class="fa fa-heart text-danger"></span> by <a href="javascript:void(0);"> nanta </a> All rights reserved
                         </div>
                     </div>
                 </div>
